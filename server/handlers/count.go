dc9abec6ca0554b7f4d793fc0d45ebad07615150
David Liu <davidliu2117@gmail.com>
2025-05-19 23:11:20 -0700

    Added count incrementing function
    Summary: added endpoint and new handler to increment current database count value by 1, and adds new postrouter to servemux

--- server/handlers/count.go
+++ server/handlers/count.go
@@ -13,6 +13,12 @@ type Count struct {
 	store *data.Store
 }
 
+type APIResponse struct {
+	Success bool `json:"success"`
+	Message string `json:"message"`
+	Data interface{} `json:"data,omitempty"`
+}
+
 func NewCount(l *log.Logger, store *data.Store) *Count {
 	return &Count{l, store}
 }
@@ -24,5 +30,28 @@ func (c *Count) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	rw.WriteHeader(http.StatusOK)
 	json.NewEncoder(rw).Encode(map[string]string{
 		"message": "successfully connected to count handler",
+	})
+}
+
+func (c *Count) IncrementCount(rw http.ResponseWriter, r *http.Request) {
+	newCount, err := c.store.IncrementCount(r.Context())
+	if err != nil {
+		c.l.Printf("Error incrementing count: %v", err)
+		rw.Header().Set("Content-Type", "application/json")
+		rw.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(rw).Encode(APIResponse{
+			Success: false,
+			Message: "Error incrementing count in database",
+			Data: nil,
+		})
+		return
+	}
+
+	rw.Header().Set("Content-Type", "application/json")
+	rw.WriteHeader(http.StatusOK)
+	json.NewEncoder(rw).Encode(APIResponse{
+		Success: true,
+		Message: "Successfully incremented count",
+		Data: newCount,
 	})
 }
